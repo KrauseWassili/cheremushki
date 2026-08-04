49d962436558f2297c5a55a166f8331b0145c608
Evgeny Eysner <evgeny.eysner@gmail.com>
2026-08-04 06:21:39 +0200

    fix(backend): remove custom number

--- backend/apps/accounts/managers.py
+++ backend/apps/accounts/managers.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.base_user import BaseUserManager
 
-from apps.accounts.services.user_service.customer_numbers import allocate_customer_number
-
 
 class UserManager(BaseUserManager):
     """Custom user manager using email as the unique identifier."""
@@ -15,8 +13,6 @@ class UserManager(BaseUserManager):
             raise ValueError("Email is required.")
 
         email = self.normalize_email(email)
-        if not extra_fields.get("customer_number"):
-            extra_fields["customer_number"] = allocate_customer_number()
         user = self.model(email=email, **extra_fields)
 
         if password:
